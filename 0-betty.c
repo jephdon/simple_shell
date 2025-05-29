caa987e622f5271f507f175331242e3feca8cc73
jephdon <chukwujekwujephthah@gmail.com>
2025-05-29 16:30:31 -0700

    Code passes betty checks

---
+++ 0-betty.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+/**
+ * main - Prints a message to the standard output using write
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	const char *msg = "Hello, Betty!\n";
+	ssize_t len = 0;
+
+	while (msg[len] != '\0')
+		len++;
+	if (write(STDOUT_FILENO, msg, len) == -1)
+		return (1);
+	return (0);
+}
