184e14eb0b2a70ffc0330e883d435f7c32481f6b
jephdon <chukwujekwujephthah@gmail.com>
2025-05-29 18:10:37 -0700

    UNIX command line interpreter

--- simple_shell.c
+++ simple_shell.c
@@ -6,6 +6,27 @@
 
 #define PROMPT "#cisfun$ "
 #define MAX_COMMAND_LENGTH 1024
+#define MAX_ARGS 64
+
+/**
+ * trim_spaces - Removes leading and trailing spaces from a string
+ * @str: The sring to trim
+ */
+
+void trim_spaces(char *str)
+{
+	char *end;
+
+	/* Trim leading spaces */
+	while (*str == ' ' || *str == '\t')
+		str++;
+	/* Trim trailing spaces */
+	end = str + strlen(str) - 1;
+	while (end > str && (*end == ' ' || *end == '\t'))
+		end--;
+	/* Null-terminate the trimmed string */
+	*(end + 1) = '\0';
+}
 
 /**
  * main - Simple Shell that reads and executes commands
@@ -16,8 +37,10 @@ int main(void)
 {
 	char command[MAX_COMMAND_LENGTH];
 	pid_t pid;
-	int status;
-	char *argv[2], *envp[1];
+	int status, i;
+	char *args[MAX_ARGS];
+	char *token;
+	char *envp[1];
 
 	while (1)
 	{
@@ -32,6 +55,11 @@ int main(void)
 		}
 		/* Remove the newline from the command */
 		command[strcspn(command, "\n")] = '\0';
+		/* Trim leading and trainling spaces */
+		trim_spaces(command);
+		/* Skip empty or space-only commands */
+		if (strlen(command) == 0)
+			continue;
 		/* Create a child process to run the command */
 		pid = fork();
 		if (pid == -1)
@@ -41,11 +69,18 @@ int main(void)
 		}
 		else if (pid == 0)
 		{
-			/* Child process: run the command */
-			argv[0] = command;
-			argv[1] = NULL;
+			/* Child process: parse and run the command */
+			i = 0;
+			token = strtok(command, " ");
+			while (token != NULL && i < MAX_ARGS - 1)
+			{
+				args[i++] = token;
+				token = strtok(NULL, " ");
+			}
+			args[i] = NULL; /* Null-terminate the argument list */
+			/* Set up environment and execute the command */
 			envp[0] = NULL;
-			if (execve(command, argv, envp) == -1)
+			if (execve(args[0], args, envp) == -1)
 			{
 				write(STDERR_FILENO, "./shell: No such file or directory\n", 35);
 				exit(1);
